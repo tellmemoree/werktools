b752684a9b2af17e177142760f79bbf23d1d818a
tellmemoree <anaythi@gmail.com>
2023-12-18 23:40:02 +0200

    Experimental parked

--- werktools/Program.cs
+++ werktools/Program.cs
@@ -2,7 +2,7 @@
 using System.Text;
 const char SEPARATOR = ',';
 // get file name from command line
-string fileName = "123.csv";
+string fileName = @"/Users/valeriizubenko/Downloads/123.csv";
 
 // read file
 string[] lines = File.ReadAllLines(fileName);
@@ -16,33 +16,32 @@ foreach (var line in lines)
 }
 
 // write file
-File.WriteAllLines("145.csv", result);
+File.WriteAllLines("/Users/valeriizubenko/Downloads/145.csv", result);
 
 string ProcessLine(string line)
 {
     var columns = line.Split(SEPARATOR);
-
-    for (var i = 0; i < columns.Length; i++)
+    string deviceType = "0D";
+    for (var i = 1; i < columns.Length; i++)
     {
         var column = columns[i];
-        if (column == "4F" && columns.Length > i + 1)
+        if (column == deviceType && columns.Length > i)
         {
-            columns[0] = columns[i];
-            columns[1] = columns[i + 1];
-            if (string.IsNullOrEmpty(columns[1])) { continue; }
-            int result = CountValues(columns[1]);
-            columns[2] = result.ToString();
+            var temp0 = columns[i];
+            var temp1 = columns[i + 1];
+
+            for (var x = 1; x < columns.Length; x += 2)
+            {
+                var shoe = columns[x];
+                if (shoe != deviceType && columns.Length > x + 1)
+                {
+                    var replace0 = temp0;
+                    var replace1 = temp1;
+
+                    break;
+                }
+            }
         }
-
     }
-
-
     return string.Join(SEPARATOR, columns);
-}
-int CountValues(string s)
-{
-    int int_value = Convert.ToInt32("FFFF", 16);
-    int int_value2 = Convert.ToInt32(s, 16);
-    int output = (int_value - int_value2);
-    return output;
 }
